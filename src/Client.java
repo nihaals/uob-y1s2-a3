356a32cfdf9c60b5bf50e3801bcb854fe03f4e4f
Nihaal Sangha <nihaal.git@gmail.com>
2022-05-16 03:47:58 +0100

    Add finished project

--- src/Client.java
+++ src/Client.java
@@ -4,16 +4,26 @@ import java.net.*;
 public class Client {
 
     public static void main(String args[]) throws IOException {
-
-        /*Open a connection to the server, create the client socket
-          System.out.println("Client is running");
-          Create I/O streams to read/write data, PrintWriter and BufferedReader
-          Read messages continuously until the user types "stop"
-             System.out.println("Enter the artist name:");
-             System.out.println("You entered " + artistName);
-             Send message to the server
-             Receive response from the server
-             System.out.println("FROM SERVER: " + serverMessage);
-          Close I/O streams and socket*/
+        String artistName, serverMessage;
+        Socket clientSocket = new Socket(Credentials.HOST, Credentials.PORT);
+        System.out.println("Client is running");
+        PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("Enter the artist name:");
+            artistName = inFromUser.readLine();
+            System.out.println("You entered " + artistName);
+            outToServer.println(artistName);
+            serverMessage = inFromServer.readLine();
+            System.out.println("FROM SERVER: " + serverMessage);
+            if (artistName.equals("stop")) {
+                break;
+            }
+        }
+        inFromServer.close();
+        inFromUser.close();
+        outToServer.close();
+        clientSocket.close();
     }
 }
--- src/Database.java
+++ src/Database.java
@@ -1,19 +1,33 @@
 import java.sql.*;
 
 public class Database {
+    private Connection con;
 
     //This method executes a query and returns the number of albums for the artist with name artistName
     public int getTitles(String artistName) {
-        int titleNum = 0;
-        //Implement this method
-        //Prevent SQL injections!
-        return titleNum;
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT COUNT(*) FROM album WHERE artistid IN (SELECT artistid FROM artist WHERE name = ?);");
+            stmt.setString(1, artistName);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     // This method establishes a DB connection & returns a boolean status
     public boolean establishDBConnection() {
-        //Implement this method
-        //5 sec timeout
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(Credentials.URL, Credentials.USERNAME, Credentials.PASSWORD);
+            return con.isValid(5);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
