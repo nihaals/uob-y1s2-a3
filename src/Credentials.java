440efd8cf340ffa725cacb796825612846f22778
Nihaal Sangha <nihaal.git@gmail.com>
2022-05-16 02:28:35 +0100

    Add template

---
+++ src/Credentials.java
@@ -0,0 +1,11 @@
+//Replace these with your own!
+
+public class Credentials {
+    //JDBC connection
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String URL = "";
+    //Client-server connection
+    public static final String HOST = "127.0.0.1"; //localhost
+    public static final int PORT = 80;
+}
